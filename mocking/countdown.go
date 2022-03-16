b08e6d61514af0108eac996da2117ce6dab64224
Ingvi Rafn Hafþórsson <ingvih@ccpgames.com>
2022-03-16 12:47:21 +0000

    Add ability to countdown from 3

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -1,4 +1,4 @@
-package countdown
+package main
 
 import (
 	"fmt"
@@ -6,5 +6,8 @@ import (
 )
 
 func Countdown(out io.Writer) {
-	fmt.Fprint(out, "3")
+    for i := 3; i > 0; i-- {
+        fmt.Fprintln(out, i)
+    }
+    fmt.Fprint(out, "Go!")
 }
